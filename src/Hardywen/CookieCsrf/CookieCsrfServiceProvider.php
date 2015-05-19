5f8ca777e795869998399ffbff9978f4e580c960
hardywen <375480616@qq.com>
2015-05-19 11:55:32 +0800

    debug

--- src/Hardywen/CookieCsrf/CookieCsrfServiceProvider.php
+++ src/Hardywen/CookieCsrf/CookieCsrfServiceProvider.php
@@ -2,14 +2,15 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class CookieCsrfServiceProvider extends ServiceProvider {
+class CookieCsrfServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
     /**
      * Bootstrap the application events.
@@ -22,37 +23,32 @@ class CookieCsrfServiceProvider extends ServiceProvider {
 
         $config = $this->app->config->get('cookie-csrf::config');
 
-        $white_list_patten = implode(',',$config['white_list']);
-
-
         //根据config配置哪些route及提交方法需要调用cookie-csrf
-        $this->app->router->when($white_list_patten, 'cookie-csrf', $config['method']);
+        $this->app->router->when('*', 'cookie-csrf', $config['method']);
 
         //将csrf token 放进 cookie里
-        \Cookie::queue('cookie_csrf_token',csrf_token());
+        \Cookie::queue('cookie_csrf_token', csrf_token());
 
     }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
         $app = $this->app;
 
         //创建 cookie-csrf filter
-        $app->router->filter('cookie-csrf', function()
-        {
-            $config = $this->app->config->get('cookie-csrf::config');
-            $black_list_patten = implode(',',$config['black_list']);
-            $current_route = $this->app->router->getCurrentRoute();
+        $app->router->filter('cookie-csrf', function () {
 
-            if(!str_is($black_list_patten,$current_route)){
-                if (\Session::token() !== \Cookie::get('cookie_csrf_token'))
-                {
-                    \Session::regenerateToken(); //token用过一次后就重新生成，防止表单重复提交
+            $config = $this->app->config->get('cookie-csrf::config');
+
+            dd($this->match($config['white_list']) , !$this->match($config['black_list']));
+            if ($this->match($config['white_list']) && !$this->match($config['black_list'])) {
+
+                if (\Session::token() !== \Cookie::get('cookie_csrf_token')) {
                     throw new \Illuminate\Session\TokenMismatchException;
                 }
 
@@ -60,16 +56,29 @@ class CookieCsrfServiceProvider extends ServiceProvider {
             }
 
         });
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('cookie-csrf');
-	}
+    private function match($pages){
+
+        $current_route = $this->app->request->path();
+
+        foreach($pages as $page){
+            if(str_is($page,$current_route)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('cookie-csrf');
+    }
 
 }
